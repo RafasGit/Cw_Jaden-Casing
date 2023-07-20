32a54bb003e20405e62ba40a58f97ef35817d9eb
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-21 00:31:34 +0300

    js file

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 String.prototype.toJadenCase = function () {
-    //...
+    //...we split the array into words, then remove the first character by slicing and then join the words together
     return this.split(' ')
      .map((x)=>
      x[0].toUpperCase() + x.slice(1)).join(' ');
   };
+  
